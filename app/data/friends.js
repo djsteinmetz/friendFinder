7b8155569a4e3a634fc537fbcfd6f5f232697a66
Ansel Adams <dj@volaciousbrands.com>
2018-08-03 20:17:08 -0500

    add more friends to api

--- app/data/friends.js
+++ app/data/friends.js
@@ -19,6 +19,56 @@ var friends = [
             5,
             5
         ]
+    },
+    {
+        name: "Nicole Brasaemle",
+        image: "https://avatars0.githubusercontent.com/u/37194559?s=400&v=4",
+        scores: [
+            5,
+            5,
+            1,
+            4
+        ]
+    },
+    {
+        name: "Tsimnuj Hawj",
+        image: "https://avatars2.githubusercontent.com/u/25711852?s=400&v=4",
+        scores: [
+            1,
+            2,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Tunji Akanbi",
+        image: "https://avatars1.githubusercontent.com/u/32908023?s=400&v=4",
+        scores: [
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Virginia de la Riva",
+        image: "https://avatars1.githubusercontent.com/u/37642808?s=400&v=4",
+        scores: [
+            5,
+            5,
+            1,
+            5
+        ]
+    },
+    {
+        name: "Lindsay Lindner",
+        image: "https://avatars2.githubusercontent.com/u/37348450?s=400&v=4",
+        scores: [
+            5,
+            5,
+            1,
+            2
+        ]
     }
 ];
 
